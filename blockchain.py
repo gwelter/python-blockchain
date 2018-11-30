d3881efb97734ca113c8dd741873733aab66ad8d
Guilherme André Welter <guilherme.a.welter@gmail.com>
2018-11-30 16:22:41 -0200

    Refactoring the verification

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,7 @@ from utility.hash_util import hash_block
 from utility.verification import Verification
 from block import Block
 from transaction import Transaction
+from wallet import Wallet
 
 MINING_REWARD = 10
 blockchain = []
@@ -66,7 +67,7 @@ class Blockchain:
                 updated_blockchain = []
                 for block in blockchain:
                     converted_tx = [Transaction(
-                        tx["sender"], tx["recipient"], tx["amount"]) for tx in block["transactions"]]
+                        tx["sender"], tx["recipient"], tx["signature"], tx["amount"]) for tx in block["transactions"]]
                     updated_block = Block(
                         block['index'], block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
                     updated_blockchain.append(updated_block)
@@ -77,7 +78,7 @@ class Blockchain:
                 updated_transactions = []
                 for tx in open_transactions:
                     updated_transaction = Transaction(
-                        tx["sender"], tx["recipient"], tx["amount"])
+                        tx["sender"], tx["recipient"], tx["signature"], tx["amount"])
                     updated_transactions.append(updated_transaction)
                 self.__open_transactions = updated_transactions
         except (IOError, IndexError):
@@ -120,12 +121,12 @@ class Blockchain:
         return self.__chain[-1]
 
 
-    def add_transaction(self, recipient, sender, amount=1.0):
+    def add_transaction(self, recipient, sender, signature, amount=1.0):
         """ Append a new value as well as the last blockchain value to the blockchain"""
         if self.hosting_node == None:
             return False
 
-        transaction = Transaction(sender, recipient, amount)
+        transaction = Transaction(sender, recipient, signature, amount)
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
@@ -139,8 +140,11 @@ class Blockchain:
 
         block_hash = hash_block(self.__chain[-1])
         proof = self.proof_of_work()
-        reward_transaction = Transaction("MINING", self.hosting_node, MINING_REWARD)
+        reward_transaction = Transaction("MINING", self.hosting_node, '', MINING_REWARD)
         copied_transactions = self.__open_transactions[:]
+        for tx in copied_transactions:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), block_hash, copied_transactions, proof)
         self.__chain.append(block)
--- utility/verification.py
+++ utility/verification.py
@@ -1,4 +1,5 @@
 from utility.hash_util import hash_block, hash_string_sha256
+from wallet import Wallet
 
 class Verification:
     @classmethod
@@ -14,14 +15,17 @@ class Verification:
         return True
 
     @staticmethod
-    def verify_transaction(transaction, get_balance):
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
+    def verify_transaction(transaction, get_balance, check_fund=True):
+        if check_fund:
+            sender_balance = get_balance()
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
 
 
     @classmethod
     def verify_transactions(cls, open_transactions, get_balance):
-        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
 
 
     @staticmethod
